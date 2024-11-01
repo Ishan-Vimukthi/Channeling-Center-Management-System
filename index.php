ff8e5fbd96425577f0ffb7941eaee20def35919f
IshanVimukthi <75219861+Ishan-Vimukthi@users.noreply.github.com>
2024-11-01 23:54:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 
 <?php
 include_once('hms/include/config.php');
-if(isset($_POST['submit']))
-{
-$name=$_POST['fullname'];
-$email=$_POST['emailid'];
-$mobileno=$_POST['mobileno'];
-$dscrption=$_POST['description'];
-$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
-echo "<script>alert('Your information succesfully submitted');</script>";
-echo "<script>window.location.href ='index.php'</script>";
 
-} ?>
+if (isset($_POST['submit'])) {
+    $name = $_POST['fullname'];
+    $email = $_POST['emailid'];
+    $mobileno = $_POST['mobileno'];
+    $dscrption = $_POST['description'];
+
+    // Prepare the stored procedure call
+    $stmt = $con->prepare("CALL InsertContactInfo(?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $name, $email, $mobileno, $dscrption);
+
+    // Execute the statement
+    if ($stmt->execute()) {
+        echo "<script>alert('Your information successfully submitted');</script>";
+        echo "<script>window.location.href ='index.php'</script>";
+    } else {
+        echo "<script>alert('Error: " . $stmt->error . "');</script>";
+    }
+
+    // Close the statement
+    $stmt->close();
+}
+?>
 <!doctype html>
 <html lang="en">
 
