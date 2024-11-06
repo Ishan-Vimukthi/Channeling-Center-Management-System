d589146b1e0c5495134b4640a0069e2c78c04f0a
IshanVimukthi <75219861+Ishan-Vimukthi@users.noreply.github.com>
2024-11-06 12:04:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,17 +27,7 @@ if (isset($_POST['submit'])) {
 <!doctype html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title> Heladiwa </title>
 
-    <link rel="shortcut icon" href="assets/images/fav.jpg">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
-</head>
 
     <body>
 
